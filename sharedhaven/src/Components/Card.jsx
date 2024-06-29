1f574454676f3022a8c2e90aba086af30879fa6a
kxrxn08 <karan.patel.kpau@gmail.com>
2024-06-29 15:02:16 +0530

    home page cards

--- sharedhaven/src/Components/Card.jsx
+++ sharedhaven/src/Components/Card.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import img1 from "../Images/workspace.webp";
-const Card = () => {
+import img2 from "../Images/14.webp"
+import img3 from "../Images/5-1.webp"
+import img4 from "../Images/9.webp"
+import img5 from "../Images/6.webp"
+
+const Card = ({id,title,content,image2}) => {
   return (
     <div className='container mt-3 '>
         <div className="row border-solid border-4 border-gray-500">
   <div className='col col-lg-6' >
-    <img src={img1} alt=""  className='h-100 w-100'/>
+    <img src={image2} alt="34444"  className='h-100 w-100'/>
   </div>
   <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 justify-between leading-normal
    col col-lg-6
    ">
     <div className="mb-8">
-      <div className="text-gray-900 font-bold text-xl mb-2">Can coffee make you a better developer?</div>
-      <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
+      <div className="text-gray-900 font-bold text-xl mb-2">{title}</div>
+      <p className="text-gray-700 text-base">{content}</p>
     </div>
     <div className="flex items-center">
       <img className="w-10 h-10 rounded-full mr-4" alt="Avatar of Jonathan Reinink"/>

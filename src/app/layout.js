9244dc7175ac022990a044d3b7335d35eddec92c
Div patel <divpatel2003@gmail.com>
2024-06-29 10:50:52 +0530

    first init

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +15,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <AppRouterCacheProvider>
+      <ThemeProvider theme={theme}>
+      <body className={inter.className} style={{backgroundColor:"white"}}>{children}</body>
+      </ThemeProvider>
+      </AppRouterCacheProvider>
     </html>
   );
 }
